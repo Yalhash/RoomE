78500b4803fd5e9ae40be02d04549b6ee11be07a
Room-E_Team <yousefalhashemi20@gmail.com>
2022-03-26 21:50:42 -0400

    update test driver code

--- test.cpp
+++ test.cpp
@@ -18,54 +18,7 @@
 #include "ArduinoSerial.h"
 #include "Lidar_MRPT.h"
 #include "RoomeMap.h"
-
-//navtesting includes
-#include <mrpt/gui/CDisplayWindow.h>
-#include <mrpt/system/filesystem.h>
-
-using namespace mrpt;
-using namespace mrpt::maps;
-using namespace mrpt::random;
-using namespace mrpt::gui;
-using namespace mrpt::math;
-using namespace mrpt::img;
-using namespace std;
-
-int navTesting(){
-    filename = "assets/Env_asset2.png";
-    printf("starting\n");
-    printf("\n");
-    fflush(stdout);
-
-    mrpt::maps::COccupancyGridMap2D occ_grid;
-	    
-
-    
-    if (occ_grid.loadFromBitmapFile(filename, 0.5f)){
-    	cout << "Building Voronoi diagram...\n";
-        occ_grid.buildVoronoiDiagram(0.5f, 0.3f);
-
-        CImage img_grid;
-        occ_grid.getAsImage(img_grid);
-
-        CImage img_voronoi;
-        CMatrixDouble mat_voronoi;
-        occ_grid.getVoronoiDiagram().getAsMatrix(mat_voronoi);
-        img_voronoi.setFromMatrix(mat_voronoi, false /* do normalization */);
-
-        // Show results:
-        CDisplayWindow win1("Grid map");
-        win1.showImage(img_grid);
-
-        CDisplayWindow win2("Voronoi map");
-        win2.showImage(img_voronoi);
-
-        mrpt::system::pause();
-    }
-
-
-
-}
+#include "DriveTrain.h"
 
 int demo() {
     printf("starting\n");
@@ -76,14 +29,14 @@ int demo() {
     lidar.initialize();
 	
     RoomeMap r_map;
+    DriveTrain d_train;
 
     // Insert the base observation
     auto scan1 = lidar.scan();
     r_map.insert_observation(scan1, mrpt::poses::CPose2D(0,0,0));
-    occ_grid = r_map.get_grid_map();
     while (true) {
         std::string user_input, y, phi;
-        std::cout << "enter \"x y phi\" change relative to the last pose" << std::endl;
+        std::cout << "enter \"x y\" change relative to the last pose" << std::endl;
         std::cout << "or enter \"s <filename>\" to save the current map to a file" << std::endl;
         std::cout << "or enter \"q\" to quit" << std::endl;
         std::cin >> user_input;
@@ -95,8 +48,10 @@ int demo() {
             std::cout << "-> Saving current map as out/" << user_input << std::endl;
             r_map.save_points_to_file(user_input);
         } else {
-            std::cin >> y >> phi;
-            mrpt::poses::CPose2D pose_delta(std::stof(user_input),std::stof(y), std::stof(phi));
+            std::cin >> y;
+            mrpt::math::TPoint2D move_point(std::stof(user_input),std::stof(y));
+	    auto pose_delta = d_train.move(r_map.get_pose(), move_point);
+
 
             // Take scan 
             auto currScan = lidar.scan();
@@ -109,5 +64,5 @@ int demo() {
 }
 
 int main(int argc, char *argv[]) {
-    return navTesting();
+    return demo();
 }
--- testing_environments.cpp
+++ testing_environments.cpp
@@ -1,9 +1,8 @@
 #include "RoomeMap.h"
 #include "TestEnv.h"
-
-int main() {
+int user_test() {
     RoomeMap r_map;
-    TestEnv env("assets/SimpleEnv.bmp");
+    TestEnv env("assets/Drawing.png");
 
     // Insert the base observation
     auto scan1 = env.scan();
@@ -33,6 +32,38 @@ int main() {
             r_map.save_grid_to_file(output_name);
         }
         ++count;
+    }
+    return 0;
+}
+
+int main() {
+    RoomeMap r_map;
+    TestEnv env("assets/SimpleEnv.bmp");
+
+    // Insert the base observation
+    auto scan1 = env.scan();
+    r_map.insert_observation(scan1, mrpt::poses::CPose2D(0,0,0));
+    int count = 0;
+    while (true) {
+
+        mrpt::poses::CPose2D pose_delta(0, 3, 0);
+        std::cout << "Moving: " << pose_delta << std::endl;
+
+        // Update location exactly 
+        env.update_pose(r_map.get_pose());
+        // Take scan
+        auto currScan = env.scan();
+        // insert the scan and update the map
+        r_map.insert_observation(currScan, pose_delta);  
+        // save scan
+        std::cout << r_map.get_pose() << std::endl;
+        std::string output_name = "scan_" + std::to_string(count);
+        std::cout << "-> Saving current map as build/" << output_name << std::endl;
+        r_map.save_grid_to_file(output_name);
+        ++count;
+        if (count > 30) {
+            break;
+        }
     }
     return 0;
 }
