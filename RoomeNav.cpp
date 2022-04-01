876a815066eb4045e5ce6adb181ebeb9e318cbe9
Ather Hassan <atherhassan@gmail.com>
2022-03-31 20:02:12 -0400

    beggining of new nav method added

--- RoomeNav.cpp
+++ RoomeNav.cpp
@@ -44,18 +44,24 @@ namespace {
         }
     }
 
+    // Check roome sized area ,returns -1 if there is an object there and avg uncertainty value otherwise
     double find_area_uncertainty(const mrpt::maps::COccupancyGridMap2D& grid, 
                                  int x, int y, int num_rows, int num_cols) {
         int count = 0;
         double sum;
-        // 5 wide square area average
-        for (int i = -2; i <= 2; ++i) {
-            for (int j = -2; j <= 2; ++j) {
+        // 9 wide square area average
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < 10; ++j) {
+                //if out of range that means we cannot move here
                 if ((x + i < 0 || x + i >= num_cols) || 
                     (y + j < 0 || y + j >= num_rows)) {
-                  continue;
+                  return -1;
                 }
                 auto prob = grid.getCell(x + i, y + j);
+                // Cannot move here if there is a guaranteed object
+                if (std::abs(prob) < 0.05){
+                    return -1;
+                }
                 sum += prob;
                 ++count;
             }
@@ -64,38 +70,47 @@ namespace {
     }
 }
 
-
+//probably want to keep track of direction we came from so we dont go there
 std::optional<mrpt::math::TPoint2D> RoomeNav::find_destiny(
         const mrpt::maps::COccupancyGridMap2D& grid,
         const mrpt::poses::CPose2D& start) {
     // Get our starting point
-    int x_ind =  grid.x2idx(start.m_coords[0]);
-    int y_ind =  grid.y2idx(start.m_coords[1]);
-    std::deque<std::pair<int,int>> que;
-    std::set<std::pair<int,int>> seen;
-    que.push_back(std::make_pair(x_ind, y_ind));
+    int curr_x =  start.m_coords[0];
+    int curr_y =  start.m_coords[1];
+
     int num_rows = grid.getSizeX();
     int num_cols = grid.getSizeY();
-    while (!que.empty()) {
-        auto p = que.front();
-        que.pop_front();
-        get_nearby_points(que,seen,p.first,p.second,num_rows,num_cols);
-        auto prob = find_area_uncertainty(grid, p.first, p.second, num_rows, num_cols);
-        auto cell_prob = grid.getCell(p.first, p.second);
-        if (std::abs(cell_prob) < 0.05) {
-            continue;
-        }
-        // If we have found an uncertain value, return that point
-        if (std::abs(prob - 0.5) < 0.05) { 
-            mrpt::math::TPoint2D ret_val(grid.idx2x(p.first), grid.idx2y(p.second));
-            return  std::optional<mrpt::math::TPoint2D>{ret_val};
-        // If we have found a wall (probability 0), dont go further
-        } else if (std::abs(prob) < 0.05){ // check if the que is empty here, if it is we go to anywhere without obstacle
-            continue;
-        }
-        //get_nearby_points(que, seen, p.first, p.second, num_rows, num_cols);
+
+    double min_uncertainty = 1;
+
+    std::deque<std::pair<int,int,int>> que;
+
+
+    //forward
+    double area_uncertainty = find_area_uncertainty(grid, grid.x2idx(curr_x - 0.3) ,grid.y2idx(curr_y + 0.3) ,num_rows,num_cols);
+
+    if (area_uncertainty != -1 && area_uncertainty < min_uncertainty){
+        mrpt::math::TPoint2D ret_val(curr_x, curr_y + 0.3);
+        return  std::optional<mrpt::math::TPoint2D>{};
     }
-    // BFS out from where we are
+
+    //left 
+    area_uncertainty = find_area_uncertainty(grid, grid.x2idx(curr_x - 0.3) ,grid.y2idx(curr_y + 0.3) ,num_rows,num_cols);
+
+    if (area_uncertainty != -1){
+        mrpt::math::TPoint2D ret_val(curr_x, curr_y + );
+        return  std::optional<mrpt::math::TPoint2D>{ret_val};
+    }
+    
+
+    //four starting points are x - 0.15
+    que.push_back()
+
+    std::set<std::pair<int,int>> seen;
+    que.push_back(std::make_pair(x_ind, y_ind));
+
+
+
     
     return std::nullopt; // We are done...
 }
