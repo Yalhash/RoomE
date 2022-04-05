019e68e0367844505ab1ae6654bb16a0dea7f4ad
Yalhash <alhashey@mcmaster.ca>
2022-04-04 20:46:53 -0400

    points are working

--- RoomeMap.cpp
+++ RoomeMap.cpp
@@ -4,40 +4,50 @@
 #include <mrpt/gui/CDisplayWindow3D.h>
 
 RoomeMap::RoomeMap() {
-    current_pose = mrpt::poses::CPose2D(0.0f,0.0f,M_PI/2);
-    running_map.changeCoordinatesReference(current_pose);
+    current_pose = mrpt::poses::CPose2D(0.0f,0.0f,0);
+    //running_map.changeCoordinatesReference(current_pose);
     // TODO: update in insert_observation based on how much we've moved
     ICP.options.maxIterations = 100;
-    ICP.options.thresholdAng = mrpt::utils::DEG2RAD(45.0f);
-    ICP.options.thresholdDist = 0.17f;
+    ICP.options.thresholdAng = mrpt::utils::DEG2RAD(10.0f);
+    ICP.options.thresholdDist = 0.75f;
     ICP.options.ALFA = 0.5f;
-    ICP.options.smallestThresholdDist = 0.01f;
+    ICP.options.smallestThresholdDist = 0.02f;
     ICP.options.doRANSAC = true;
-    ICP.options.ICP_algorithm = mrpt::slam::icpClassic;
+    ICP.options.ICP_algorithm =mrpt::slam::icpLevenbergMarquardt;// mrpt::slam::icpClassic;
 }
 
 void RoomeMap::insert_observation(const mrpt::obs::CObservation2DRangeScan& scan,
                                   const mrpt::poses::CPose2D& pose_delta_approx) {
-    //should not be off by more than we actually moved
-    /* ICP.options.thresholdAng = mrpt::utils::DEG2RAD(pose_delta_approx.phi()); */
-    /* ICP.options.thresholdDist = sqrt(pose_delta_approx.m_coords[0]*pose_delta_approx.m_coords[0] + pose_delta_approx.m_coords[1]*pose_delta_approx.m_coords[1]); */
+
+    /*    if (running_map.isEmpty()){
+        std::cout << "First observation, icp not required" << std::endl;
+        running_map.insertObservation(&scan);
+        mrpt::poses::CPose3D acurrent3D(current_pose);
+        running_grid.insertObservation(&scan,&acurrent3D);
+        return;
+    }*/
     mrpt::maps::CSimplePointsMap curr_map;
-    curr_map.changeCoordinatesReference(current_pose + pose_delta_approx);
     /* mrpt::poses::CPose3D obs_pose(current_pose + pose_delta_approx); */
+/*    curr_map.save2D_to_text_file("icpBS/beforePts.txt");
+    curr_map.changeCoordinatesReference(pose_delta_approx + current_pose);
+    curr_map.save2D_to_text_file("icpBS/refChange.txt");*/
+    curr_map.changeCoordinatesReference(mrpt::poses::CPose2D(0,0,-pose_delta_approx.phi() - current_pose.phi()));
+
     curr_map.insertObservation(&scan);
+
+    running_map.changeCoordinatesReference(mrpt::poses::CPose2D(0,0,- current_pose.phi()));
     /* curr_map.insertObservation(&scan, &obs_pose); */
     //NOTE: may want run info in future to check goodness etc.
     mrpt::poses::CPosePDF::Ptr pdf = ICP.Align(
             &running_map, 
             &curr_map,
-            pose_delta_approx, // pose of m2 relative to m1
+            mrpt::poses::CPose2D(pose_delta_approx.m_coords[0],pose_delta_approx.m_coords[1],0), // pose of m2 relative to m1
             nullptr, // runtime
             &info); // run info 
 
     std::cout << "ICP iterations: " << info.nIterations << " goodness: " << info.goodness << std::endl;
     
     // update current pose to where we think we are
-    std::cout << "cpose b4: " << current_pose << std::endl;
     auto mean_pose = pdf->getMeanVal(); 
 
     //If icp algorithm isnt good enough use odometry
@@ -45,13 +55,38 @@ void RoomeMap::insert_observation(const mrpt::obs::CObservation2DRangeScan& scan
         /* current_pose.m_coords[0] += mean_pose.m_coords[0]; */
         /* current_pose.m_coords[1] += mean_pose.m_coords[1]; */
         /* current_pose.phi_incr(mean_pose.phi()); */
-        current_pose += mean_pose;
-
+        std::cout << "cpose: " << current_pose << std::endl;
+        std::cout << "mean pose: " << mean_pose << std::endl;
+        std::cout << "delta: " << pose_delta_approx << std::endl;
+    
+    std::cout << "curr pose + delta pose: " << current_pose+pose_delta_approx << std::endl;
+        std::cout <<  " current + mean: " << current_pose+mean_pose << std::endl;
+        std::cout << "current - mean: " << current_pose - mean_pose << std::endl;
+        
+        std::cout << "mean -current: " << mean_pose - current_pose << std::endl;
+        
+        std::cout << " mean + current: " << mean_pose + current_pose << std::endl;
+        std::cout << "-current + mean: " << -current_pose + mean_pose << std::endl;
+        std::cout << "-current - mean: " << -current_pose - mean_pose << std::endl;
+        
+        
+       /* current_pose.m_coords[0] += pose_delta_approx.m_coords[0];
+        current_pose.m_coords[1] += pose_delta_approx.m_coords[1];
+        current_pose.phi_incr(pose_delta_approx.phi());*/
+        
+        std::cout << "--------------------------------------------" << std::endl;
         // correct our scan
-        curr_map.changeCoordinatesReference(mean_pose-pose_delta_approx);
-        running_map.changeCoordinatesReference(mean_pose);
+        //curr_map.changeCoordinatesReference(mean_pose);
         // update the current map and grid
+
+        running_map.changeCoordinatesReference(-mean_pose);
         running_map.fuseWith(&curr_map);
+       // save_points_to_file(std::to_string(count)+"_Fused");
+        running_map.changeCoordinatesReference(mrpt::poses::CPose2D(0,0,current_pose.phi()));
+        save_points_to_file("Fused_adjusted");
+        //count ++;
+       current_pose = mean_pose + current_pose;
+       /* mrpt::poses::CPose3D current_3D( mrpt::poses::CPose2D(running_grid.x2idx(current_pose.m_coords[0]),running_grid.x2idx(current_pose.m_coords[1]),current_pose.phi())); */
         mrpt::poses::CPose3D current_3D(current_pose);
         running_grid.insertObservation(&scan,&current_3D);
     }else{
@@ -61,7 +96,7 @@ void RoomeMap::insert_observation(const mrpt::obs::CObservation2DRangeScan& scan
         std::cout << "bad icp. Discarding and using odometry values" << std::endl;
     }
 
-    std::cout << "cpose: " << current_pose << " pdf mean: "  << mean_pose << " pose_delta_approx " << pose_delta_approx<< std::endl;
+    //std::cout << "cpose: " << current_pose << " pdf mean: "  << mean_pose << " pose_delta_approx " << pose_delta_approx<< std::endl;
 }
 
 
